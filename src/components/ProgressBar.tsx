20610f3d23ea8efd7bd262fac076b642df89d406
Schuyler Alschuler <alschuler.s@gmail.com>
2023-08-21 21:50:30 -0700

    Calendar progress

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,15 +1,45 @@
 import "./ProgressBar.css";
 
-const ProgressBar = () => (
-  <div className="progress-item">
-    <div className="progress-title">
-      <div className="progress-title-left">❤️ Love</div>
-      <div className="progress-title-right">12 seconds</div>
+type props = {
+  title: String;
+  time: Date;
+  getPercentage: (time: Date) => number;
+  getTimeLeft: (time: Date) => number;
+  singularSuffix: String;
+  allSingular?: Boolean;
+};
+
+const ProgressBar = ({
+  title,
+  time,
+  getPercentage,
+  getTimeLeft,
+  singularSuffix,
+  allSingular,
+}: props) => {
+  const getSuffix = () => {
+    if (!!allSingular) {
+      return singularSuffix;
+    }
+    return `${singularSuffix}${getTimeLeft(time) === 1 ? "" : "s"}`;
+  };
+
+  return (
+    <div className="progress-item">
+      <div className="progress-title">
+        <div className="progress-title-left">{title}</div>
+        <div className="progress-title-right">
+          {`${getTimeLeft(time)} ${getSuffix()}`}
+        </div>
+      </div>
+      <div className="progress-outer">
+        <div
+          className="progress-inner"
+          style={{ width: `${getPercentage(time)}%` }}
+        ></div>
+      </div>
     </div>
-    <div className="progress-outer">
-      <div className="progress-inner"></div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ProgressBar;
