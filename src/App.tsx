7f6850d5988bc4cd6bffdbd3b04bb45c694c3e04
Schuyler Alschuler <alschuler.s@gmail.com>
2023-08-21 20:31:23 -0700

    Initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,14 @@ import ProgressBar from "./components/ProgressBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="app-title-container">
+        <div className="app-title">Hi there</div>
+      </div>
+      <div className="progress-bars">
         <ProgressBar />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ProgressBar />
+      </div>
     </div>
   );
 }
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,3 +1,15 @@
-const ProgressBar = () => <div>Hi</div>;
+import "./ProgressBar.css";
+
+const ProgressBar = () => (
+  <div className="progress-item">
+    <div className="progress-title">
+      <div className="progress-title-left">❤️ Love</div>
+      <div className="progress-title-right">12 seconds</div>
+    </div>
+    <div className="progress-outer">
+      <div className="progress-inner"></div>
+    </div>
+  </div>
+);
 
 export default ProgressBar;
